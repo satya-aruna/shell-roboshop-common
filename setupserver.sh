a838b6fbca5ca7df8da41f0ddf855a07f4aeb246
Satya Aruna <satya.aruna@gmail.com>
2026-02-04 19:56:28 +0530

    files changed

--- setupserver.sh
+++ setupserver.sh
@@ -4,6 +4,15 @@ source ./installandsetup.sh
 
 SERVER=$1
 
+START_TIME=$(date +%s)
+
 INSTALL_SETUP $SERVER
+
+END_TIME=$(date +%s)
+
+TOTAL_TIME=$(($END_TIME-$START_TIME))
+
+echo "Script execution time: $TOTAL_TIME"
+
 
 
